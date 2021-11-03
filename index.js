6061ec701e1e8e4024a6ea4ea5fa80139199a662
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-03 06:50:38 -0800

    05 - Checking the type of each variable at runtime

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Open chrome --> Console --> Check each variable type by using: typeof variable
 let name = 'John'; // String Literal
 let age = 21; // Number Literal
 let isApproved = true; // Boolean Literal
