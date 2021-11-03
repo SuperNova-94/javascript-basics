07e637bdf3fdd3a7a32397cc5ef502106867ff9c
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-03 10:52:03 -0800

    06 - Objects

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-// Open chrome --> Console --> Check each variable type by using: typeof variable
-let name = 'John'; // String Literal
-let age = 21; // Number Literal
-let isApproved = true; // Boolean Literal
-let boundary = undefined;
-let selectedColor = null;
+// let name = 'John'; 
+// let age = 21;
+let person = {
+    // object literal
+    name: 'John',
+    age: 21
+};
+console.log(person);
+
+// Change name of the person //
+
+// Method 1: Dot Notation (Default)
+person.name = 'Alexander'
+console.log(person.name);
+
+// Method 2: Bracket Notation
+person['name'] = 'Jack Sparrow'
+console.log(person.name);
+
+// Method 2: Example where/how it can be used
+let gen = 'name'
+person[gen] = 'Joe'
+console.log(person.name);
