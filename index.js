3b5e8ee1d0ba1f8b5b439b11b40b5711fb5aac39
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-03 06:05:19 -0800

    02 - Variables

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-// This is my first JavaScript code
-console.log('Hello World'); 
+let who = 'John';
+console.log(who);
+
+// What you should know about variables //
+/*
+    1.Cannot be reserved keyword
+    2.Always use meaningful names
+    3.Cannot start witha number (eg: 46zigzag)
+    4.Cannot contain a space or hyphen (-)
+    5.They are case-sensitive 
+*/
+
+let emptyStr;  // Camel notation: (eg: iPod)
+
+// Avoid writing like this ✖
+let firstName = 'Peter', lastName = 'Doe';
+console.log(firstName);
+console.log(lastName);
+
+// Write like this instead [Best Practice] ✔
+let firstName1 = 'Jensen'
+let lastName1 = "Larrison"
+console.log(firstName1);
+console.log(lastName1);
