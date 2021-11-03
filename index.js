0825270ccdfc30bdca299b7e746d273b73fd0026
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-03 06:31:42 -0800

    03 - Constants

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-let who = 'John';
-console.log(who);
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
 
-// What you should know about variables //
-/*
-    1.Cannot be reserved keyword
-    2.Always use meaningful names
-    3.Cannot start witha number (eg: 46zigzag)
-    4.Cannot contain a space or hyphen (-)
-    5.They are case-sensitive 
-*/
-
-let emptyStr;  // Camel notation: (eg: iPod)
-
-// Avoid writing like this ✖
-let firstName = 'Peter', lastName = 'Doe';
-console.log(firstName);
-console.log(lastName);
-
-// Write like this instead [Best Practice] ✔
-let firstName1 = 'Jensen'
-let lastName1 = "Larrison"
-console.log(firstName1);
-console.log(lastName1);
+// If you don't want the value of a variable to change use the following method
+const interestRate1 = 0.3;
+// interestRate1 = 1;
+console.log(interestRate1);
