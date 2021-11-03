c192d248f15ea0c68fb9212ee107e7596de36181
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-03 11:41:31 -0800

    09 - Types of functions

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-function greeting() {
-    console.log('Bienvenue');
+// Calculating a value
+function square(num) {
+    return num * num
 }
-greeting(); // function call
-
-// Functions below are all performing a task //
-// Example 1:
-function greet(name){
-    console.log('Hello' + ' ' + name);  // + concatinates
-}
-let input = prompt('Enter your name');
-greet(input);
-
-// Example 2:
-function welcome(firstName, lastName) {
-    console.log('Hello' + ' ' + firstName + ' ' + lastName);
-}
-let a = prompt('Enter your first name');
-let b = prompt('Enter your last name');
-welcome(a,b);
-
-// Example 2: Without getting input from the user
-function simple(x, y) {
-    console.log(x + ' ' + y + ' just joined this server!');
-}
-simple('James', 'Nikolas');
+let input = prompt('Enter a value to square')
+console.log(square(input));
