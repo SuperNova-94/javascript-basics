b03ee20c62c5bd970199ffabc3dab51c6ed8d9d2
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-04 02:35:32 -0800

    13 - Comparison Operators

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
-/* 
-    Operators:
-        Operators Variables --> Expressions --> Algorithms
-            Types:
-                Arithmetic ✔
-                Assignment  ✔
-                Comparison
-                Logical
-                Bitwise
-*/
+let x = 1; // assign x to 1
 
-// Arithmetic //
-let x = 10;
-let y = 3;
+// Relational 
+console.log(x > 0); 
+console.log(x >= 1); 
+console.log(x < 1); 
+console.log(x <= 1); 
 
-console.log(x + y); // Addition Operator
-console.log(x - y); // Subtraction Operator
-console.log(x * y); // Multiplication Operator
-console.log(x / y); // Division Operator
-console.log(x % y); // Remainder of division Operator
-console.log(x ** y); // Exponentiation Operator
-
-// Increment (++)
-console.log('x = ' + x);
-console.log('x incremented: ' + ++x);  // Increments current value then displays
-console.log(y++); // Displays current value first and then increments
-console.log(y); // Displays the incremented value of y
-
-// Decrement (--)
-console.log('x is: ' + x);
-console.log('x is now: ' + --x);
-
-// Assignment //
-let x = 20;
-
-x = x + 5; 
-x += 5; // Alternative to line 36
-
-x = x * 3;
-x *= 3; // Alternative to line 39
+// Equality
+console.log(x === 1); // check if x is equal to 1 
+console.log(x !== 1); // check if x is not equal to 1
