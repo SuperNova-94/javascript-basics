b702b6b391fcbad5c23dd3f41db157becc1b8f0a
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-04 03:16:30 -0800

    16 - Logical Operators

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 /*
-    Scenario:
-    If a user has reached 60,000 rank points, they are 'VIP', otherwise 'Member'
+Logical AND (&&)
+Returns TRUE if both operands are TRUE
+*/
+console.log(true && false);
+
+// Example:
+let highIncome = true;
+let goodCreditScore = false;
+let eligibleForLoan = highIncome && goodCreditScore;
+
+console.log('Are you eligible for loan:', eligibleForLoan);
+
+/*
+Logical OR (||)
+Returns TRUE if one of the operands is TRUE
 */
 
-let rankPoints = prompt('Enter rank points:');
-let type = rankPoints >= 60000 ? 'VIP' : 'Member';
-console.log(type);
+//Example: 
+let highIncome2 = true;
+let goodCreditScore2 = true;
+let eligibleForLoan2 = highIncome2 || goodCreditScore2;
+
+console.log('Are you eligible for loan:', eligibleForLoan2);
+
+// NOT (!)
+let applicationRefused = !eligibleForLoan;
+console.log('Application Refused:', applicationRefused);
