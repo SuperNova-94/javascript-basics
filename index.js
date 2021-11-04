df1762ae850906f3a9c0790dde0223eee67096e5
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-04 01:36:47 -0800

    11 - Arithmetic Operators

--- index.js
+++ index.js
@@ -2,10 +2,30 @@
     Operators:
         Operators Variables --> Expressions --> Algorithms
             Types:
-                Arithmetic
+                Arithmetic ✔
                 Assignment  
                 Comparison
                 Logical
                 Bitwise
 */
 
+// Arithmetic //
+let x = 10;
+let y = 3;
+
+console.log(x + y); // Addition Operator
+console.log(x - y); // Subtraction Operator
+console.log(x * y); // Multiplication Operator
+console.log(x / y); // Division Operator
+console.log(x % y); // Remainder of division Operator
+console.log(x ** y); // Exponentiation Operator
+
+// Increment (++)
+console.log('x = ' + x);
+console.log('x incremented: ' + ++x);  // Increments current value then displays
+console.log(y++); // Displays current value first and then increments
+console.log(y); // Displays the incremented value of y
+
+// Decrement (--)
+console.log('x is: ' + x);
+console.log('x is now: ' + --x);
