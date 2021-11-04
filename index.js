173e211890bfd0d1444556eb82326b5dffe3f748
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-04 03:56:18 -0800

    19 - Operators precendence

--- index.js
+++ index.js
@@ -1,36 +1,5 @@
-// 1 = 00000001
-// 2 = 00000010
-// R = 00000011
-console.log(1 | 2); // Bitwise OR
+let x = 2 + 3 * 4;
+console.log(x);
 
-// 1 = 00000001
-// 2 = 00000010
-// 3 - 00000011
-// R = 00000000
-console.log(1 & 2); // Bitwise AND
-
-// Example: Access Control System //
-/* Read, Write, Execute
-    Read = 00000100 
-    Write = 0000010
-    Execute = 00000001 
-*/
-
-const readPermission = 4;
-const writePermission = 2; 
-const exectutePermission = 1; 
-
-let myPermission = 0;
-myPermission = myPermission | readPermission | writePermission; // Bitwise OR
-console.log(myPermission);
-
-let message = 
-    (myPermission & writePermission) ? 'Yes':'No' // Bitwise AND
-console.log(message);
-
-// Take-away:
-/* 
-    --> With the bitwise OR operator we can add permission.
-
-    --> With the bitwise AND operator we can check to see if we have a given permission.
-*/
+let y = (2 + 3) * 4; // Note: The bracket gives precedence
+console.log(y);
