6785423ea08ad1f4eaddef458614a8e76c51439c
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-04 02:45:57 -0800

    14 - Equality Operators

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-let x = 1; // assign x to 1
+// Stricts Equality: Must be of same Type + same Value [Default]
+console.log(1 === 1);
+console.log('1' === 1); // false as a string and a number is being compared
 
-// Relational 
-console.log(x > 0); 
-console.log(x >= 1); 
-console.log(x < 1); 
-console.log(x <= 1); 
-
-// Equality
-console.log(x === 1); // check if x is equal to 1 
-console.log(x !== 1); // check if x is not equal to 1
+// Lose Equality: Does not care about what types you have, will be converted to ensure equal Type. [NOTE: Will only check if the Values are equal] 
+console.log(2 == 2);
+console.log('2' == 2);
