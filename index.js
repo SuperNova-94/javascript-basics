472e6679aade6db848b79edb358b8a0ebf8f8732
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-04 04:45:23 -0800

    21 - Conditional Statements: switch...case

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-// Hour
-// If hour is between 1am and 12pm: Good morning!
-// If it's between 12pm and 6pm: Good afternoon!
-// Otherwise: Good evening!
+let myRank = 20000;
+switch (myRank) {
+    case 10000:
+        console.log('You are a member!');
+        break;
+    case 20000:
+        console.log('You are an addict!');
+        break;
+    case 60000:
+        console.log('You are VIP!');
+        break;
 
-/*  Format:
+    default:
+        console.log('Power-User')
+}
 
-    if (condition) {
-        statement
-    }
-    else if (anotherCondition) {
-        statement
-    }
-    else if (yetAnotherCondition) {
-        statement
-    } else {
-        statement
-    }
-
- */
-let hour = prompt('Enter current time in 24hr format:');
-
-if (hour >= 1 && hour < 12)
-    console.log('Good morning!');
-else if (hour >= 12 && hour < 18) 
-    console.log('Good afternoon!');
+// Personal preference:
+let x = 60000;
+if (x === 10000)
+    console.log('You are a member!');
+else if (x === 20000)
+    console.log('You are an addict!');
+else if (x === 60000)
+    console.log('You are a VIP!');
 else 
-    console.log('Good evening!');
+    console.log('Power-User')
