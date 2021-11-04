80a33ff290e9b815a64ba110c08288f597e99804
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-04 03:50:53 -0800

    18 - Bitwise Operators

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-// Falsy (false)
-// undefined
-// null
-// 0 
-// false
-//  ''
-//  NaN
+// 1 = 00000001
+// 2 = 00000010
+// R = 00000011
+console.log(1 | 2); // Bitwise OR
 
-// Anything that is not Falsy is Truthy
+// 1 = 00000001
+// 2 = 00000010
+// 3 - 00000011
+// R = 00000000
+console.log(1 & 2); // Bitwise AND
 
-// Example:
-let userColor = 'violet';
-let defaultColor = 'indigo'
-let currentColor = userColor || defaultColor;
+// Example: Access Control System //
+/* Read, Write, Execute
+    Read = 00000100 
+    Write = 0000010
+    Execute = 00000001 
+*/
 
-console.log(currentColor);
+const readPermission = 4;
+const writePermission = 2; 
+const exectutePermission = 1; 
 
-let userColor2 = null;
-let defaultColor2 = 'indigo'
-let currentColor2 = userColor2 || defaultColor2;
+let myPermission = 0;
+myPermission = myPermission | readPermission | writePermission; // Bitwise OR
+console.log(myPermission);
 
-console.log(currentColor2);
+let message = 
+    (myPermission & writePermission) ? 'Yes':'No' // Bitwise AND
+console.log(message);
+
+// Take-away:
+/* 
+    --> With the bitwise OR operator we can add permission.
+
+    --> With the bitwise AND operator we can check to see if we have a given permission.
+*/
