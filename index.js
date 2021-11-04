c7deb8b42590adfc4c81bf4a1482dbe68220ae5e
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-04 01:35:29 -0800

    10 - Operators and their types

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-// Calculating a value
-function square(num) {
-    return num * num
-}
-let input = prompt('Enter a value to square')
-console.log(square(input));
+/* 
+    Operators:
+        Operators Variables --> Expressions --> Algorithms
+            Types:
+                Arithmetic
+                Assignment  
+                Comparison
+                Logical
+                Bitwise
+*/
+
