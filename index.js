c1c5de447349338e187e080301ca67a92ed315c0
John <76906503+SuperNova-94@users.noreply.github.com>
2021-11-05 23:48:45 +0400

    21 - Conditional Statements: switch...case

--- index.js
+++ index.js
@@ -16,11 +16,15 @@ switch (myRank) {
 
 // Personal preference:
 let x = 60000;
-if (x === 10000)
+if (x === 10000) {
     console.log('You are a member!');
-else if (x === 20000)
+}
+else if (x === 20000) {
     console.log('You are an addict!');
-else if (x === 60000)
+}
+else if (x === 60000) {
     console.log('You are a VIP!');
-else 
+}
+else {
     console.log('Power-User')
+}
